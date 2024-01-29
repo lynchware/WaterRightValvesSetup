3d47b30dff94d44cfd268405e99e581de4b00adf
Tim Lynch <tlynch@hbs.net>
2024-01-29 14:03:50 -0600

    Add project files.

---
+++ WaterRightValves/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WaterRightValves
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WaterRightValves/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WaterRightValves
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
